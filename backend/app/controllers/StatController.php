e99c8dfc1ce820e88e0c881a1a218736ab0a5081
Daniel Negrete <daniel.negrete@sinneg.com>
2015-09-07 10:34:31 -0600

    casi todo

--- backend/app/controllers/StatController.php
+++ backend/app/controllers/StatController.php
@@ -5,38 +5,34 @@ class StatController extends \BaseController {
 		$period = Input::get('period');
 		$resource = Input::get('resource');
 
-		$year = explode('-',date('Y'));
-		$month = explode('-',date('m'));
-		$day = explode('-',date('d'));
+		$year = intval(date('Y'));
+		$month = intval(date('m'));
+		$day = intval(date('d'));
 
-		$stats = Stat::where('resource', $resource);
 		$data = Stat::where('resource', $resource);
 		
 		if ($period == 'dialy'){
-			$stats->whereRaw('day(created_at) = ?',[$month]);
-			$data->whereRaw('day(created_at) = ?',[$month]);
+			$data->whereRaw('day(created_at) = ?',[$day]);
+			$x = 'hour(created_at)';
 		}
+		
 		if ($period == 'dialy' || $period == 'monthly' ){
-			$stats->whereRaw('month(created_at) = ?',[$month]);
 			$data->whereRaw('month(created_at) = ?',[$month]);
-		}
-		if (!$period){
-			$stats->whereRaw('year(created_at) = ?',[$year]);
-			$data->whereRaw('year(created_at) = ?',[$year]);
+			if ($period == 'monthly' )
+				$x = 'week(created_at)';
 		}
 
-		$stats->groupBy('method');
-		$stats->select(DB::raw('count(*) as times'),'method');
+		$data->whereRaw('year(created_at) = ?',[$year]);
+		if ($period == 'yearly' )
+			$x = 'month(created_at)';
 
-		// $data->groupBy(DB::raw('month(created_at)'));
-		$data->groupBy(DB::raw('day(created_at)'),'method');
-		if ($period == 'dialy'){
-			
-		}
+		$data->groupBy(DB::raw($x), 'method');
 		$data->select(DB::raw('count(*) as times'),
 			'method', 
-			DB::raw('day(created_at) as x'));
+			DB::raw($x.' as x'));
 
-		return Response::json(['stats' => $stats->get(), 'data' => $data->get()]);
+		return Response::json([
+			'data' => $data->get(),
+			]);
 	}
 }
--- frontend/src/app/main/views/dashboard/DashboardCtrl.js
+++ frontend/src/app/main/views/dashboard/DashboardCtrl.js
@@ -1,24 +1,56 @@
 "use strict";
 angular.module('agenda')
-	.controller('DashboardCtrl', ['$rootScope', '$scope', '$routeParams', '$filter',
-		function ($rootScope, $scope, $routeParams, $filter) {
+	.controller('DashboardCtrl', ['$rootScope', '$scope', '$routeParams', 'StatService',
+		function ($rootScope, $scope, $routeParams, StatService) {
+			$scope.stats = {};
+			$scope.chart = {};
+			$scope.period = 'dialy';
+			$scope.reloadStats = function () {
+				StatService.getStats($scope.period, 'contacts').then(function (response) {
+					var stats = response.data;
 
+				 	$scope.chart.stats = [];
+				 	var format;
+				 	switch($scope.period){
+				 		case 'yearly': format = 'YYYY'; break;
+				 		case 'monthly': format = 'MMM - YYYY'; break;
+				 		case 'dialy': format = 'h a'; break;
+				 	}
+				 	_.each(_.groupBy(stats, 'x'),function(points, index){
+				 		var plot = {};
+				 		switch($scope.period){
+				 			case 'yearly': plot.y = moment().month(index).format(format); break;
+				 			case 'monthly':plot.y = moment().day(index).format(format); break;
+				 			case 'dialy':  plot.y = moment().hour(index).format(format); break;
+				 		}	
+				 		_.each(points, function(point, jindex){
+				 			plot[point.method] = point.times;
+				 		});
+				 		$scope.chart.stats.push(plot);
+				 	});
+				 	$scope.stats = _.map($scope.chart.stats, function(stat){
+				 		return {stat.method: stat.times};
+				 	});
+				 	console.log($scope.stats);
+				})
+			}
 			$scope.chart = {
-				colors: ["#31C0BE", "#C7254E"],
-				data: [{
-					y: "2006",
+				colors: ["#31C0BE", "#F7254E",'#A7F4E', '#C7254F'],
+				parseTime:false,
+				stats: [{
+					y: "5",
 					a: 100,
 					b: 90
 				}, {
-					y: "2007",
+					y: "6",
 					a: 75,
 					b: 65
 				}, {
-					y: "2008",
+					y: "7",
 					a: 50,
 					b: 40
 				}, {
-					y: "2009",
+					y: "8",
 					a: 75,
 					b: 65
 				}, {
@@ -34,6 +66,8 @@ angular.module('agenda')
 					a: 100,
 					b: 90
 				}]
-			}
+			};
+
+			$scope.reloadStats();
 		}
 	]);
